a42e4b275e15be11ec38bb17e90b3d6424174de4
Laith Odauh <LaithOudah@gmail.com>
2021-10-06 17:37:38 +0200

    Beautified Version

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import './App.css';
-import countries from 'world-countries';
-import CountryInfo from './CountryInfo';
+import React from "react";
+import "./App.css";
+import countries from "world-countries";
+import CountryInfo from "./CountryInfo";
 
 function App() {
   // Sorting counters by Area
-  let holder = countries.sort((a,b) => b.area - a.area)
+  let holder = countries.sort((a, b) => b.area - a.area);
 
   // Removing Antarctica and taking the biggest 15 countries
-  holder = holder.filter((arg) => arg.name.common !== "Antarctica").slice(0, 15);
+  holder = holder
+    .filter((arg) => arg.name.common !== "Antarctica")
+    .slice(0, 15);
 
   // Creating a countrylist
   const CountryList = holder.map((holder, index) => {
-
-
-  // Returning the countryInfo asked with conditions
+    // Returning the countryInfo asked with conditions
     return (
-  <CountryInfo
-  key ={holder.cca3}
-  data = {holder}
-  fatArea = {countries[0].area}
-  detailed = {index < 5}
-  />
-  );
-  }); 
+      <CountryInfo
+        key={holder.cca3}
+        data={holder}
+        maxArea={countries[0].area}
+        detailed={index < 5}
+      />
+    );
+  });
 
   return (
     // Calling the funciton
     <div>{CountryList}</div>
-   )}
+  );
+}
 
 export default App;
--- src/CountryInfo.js
+++ src/CountryInfo.js
@@ -1,29 +1,31 @@
 import React from "react";
 
 // Component CountryInfo containting props
-const CountryInfo = ({ data, fatArea, detailed }) => {
-    // Props
-    const Name = data.name.common;
-    const Area = data.area;
-    const Capital = data.capital;
-    const Region = data.region;
-    let Ratio = Area/fatArea;
-    let bredbror = (Ratio*80) + "%";
-    
-return (
+const CountryInfo = ({ data, maxArea, detailed }) => {
+  // Props
+  const Name = data.name.common;
+  const Area = data.area;
+  const Capital = data.capital;
+  const Region = data.region;
+  let Ratio = Area / maxArea;
+  let bredd = Ratio * 80 + "%";
 
-<div className="country">
-  <span className="countryName">{Name} </span>
-  <span> {Area} km<sup>2</sup> </span>
-
-  <div className="bar" style={{width: bredbror}}> </div>
-
-      { detailed && (
+  return (
+    <div className="country">
+      <span className="countryName">{Name} </span>
+      <span>
+        {" "}
+        {Area} km<sup>2</sup>{" "}
+      </span>
+      <div className="bar" style={{ width: bredd }}>
+        {" "}
+      </div>
+      {detailed && (
         <div>
-        Capital: {Capital} <br></br> Region: {Region}
-
-        </div> )} </div>
-
-    );
-}
+          Capital: {Capital} <br></br> Region: {Region}
+        </div>
+      )}{" "}
+    </div>
+  );
+};
 export default CountryInfo;
